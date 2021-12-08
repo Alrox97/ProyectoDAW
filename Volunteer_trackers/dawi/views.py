744e6bbfd5041949740ce34659c48783240c6d83
Israel Cruz Morales <isracruzm@hotmail.com>
2021-12-07 22:19:54 -0600

    basic login and signup

--- Volunteer_trackers/dawi/views.py
+++ Volunteer_trackers/dawi/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.db.models.manager import EmptyManager
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.views.generic.base import TemplateView
+from .forms import SignUpForm, EventsForm
+from .models import Events
+from django.views.generic import ListView, DetailView
+from datetime import datetime
+    
 
-# Create your views here.
+def home(request):
+    """
+    Renderiza una pantalla de inicio.
+    **Template:**
+    :template:`home/home.html`
+    """
+    return render(request, 'home/home.html')
+
+
+def signup(request):
+    """
+    Despliega el login de usuario.
+    **Template:**
+    :template:`authen/signup.html`
+    """
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        try:
+            if form.is_valid():
+                user = form.save()
+                user.save()
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=user.username,
+                                    password=raw_password)
+                login(request, user)
+                return redirect('/login')
+        except:
+            return redirect('/login')
+    else:
+        form = SignUpForm()
+    return render(request, 'authen/signup.html', {'form': form})
