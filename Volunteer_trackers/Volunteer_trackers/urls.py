183214b199a931aecd95ce83e4c484b14b8f967b
Israel Cruz Morales <isracruzm@hotmail.com>
2021-12-07 22:23:42 -0600

    moving up

--- Volunteer_trackers/Volunteer_trackers/urls.py
+++ Volunteer_trackers/Volunteer_trackers/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='authen/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
+     path('myprofileVolun/', auth_views.LoginView.as_view(template_name='institutions/myprofileVolun.html'), name='myprofileVolun'),
 ]
